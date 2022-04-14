cf6b274e782ddfba1ee46b1047eb2944b812dcba
SoraLinken <83252345+SoraLinken@users.noreply.github.com>
2022-04-15 01:05:54 +0300

    submit after fixes

--- ex3/script/script.js
+++ ex3/script/script.js
@@ -1,55 +1,62 @@
 let b;
 createBoxes()
 
+//plus button
 let button = document.getElementById("btn");
-let box1 = document.getElementById("box-1");
 
 button.addEventListener("click", changeOpacity())
 
+/**
+ * created flex container
+ */
 function createFlexContainer(){
+
     document.getElementById('main').innerHTML = '<div></div>';
 }
 
+/**creates one box */
 function createBox(i){
+
     b= Math.random();
     let box = document.createElement('article');
     box.setAttribute("backgroundColor", '#000000' )
     box.setAttribute("id", 'box-'+i);
     box.setAttribute("style", `opacity: ${b};`);
     box.setAttribute("onclick", "changeColor(this)")
-    document.getElementById('main').appendChild(box);}
+    document.getElementById('main').appendChild(box);
+}
 
+/**creates multiple boxes can be changed by changeing the for loop */
 function createBoxes(){
+    
     for (let i = 1; i < 43; i++) {
         createBox(i)
     }
 }
 
+/**
+ * changes the color of a clicked box
+ * @param {Object} box 
+ */
 function changeColor(box){
-    console.log("bla")
-    console.log(box)
-    console.log(box.id)
-    console.log(box.style)
-    document.getElementById(box)
 
     let boxColor = box.style
     
-    console.log(boxColor)
     if (boxColor.backgroundColor == "rgb(255, 255, 255)") {
         boxColor.backgroundColor = "#000000";
-        console.log("yes");
     } else {
         boxColor.backgroundColor = "#FFFFFF";
-        console.log("no");
 
     }
-    console.log(box.style.opacity)
 }
 
+/**
+ * increaces the opacity of the first box.
+ */
 function changeOpacity(){
-    box = document.getElementById("box-1")
-    console.log("bla")
-    console.log(box.style.opacity)
+
+    let box = document.getElementById("box-1")
+
     let boxOpacity = box.style.opacity
     let check = 0.05 + parseFloat(boxOpacity)
     if (check<=1){
@@ -59,11 +66,3 @@ function changeOpacity(){
         box.style.opacity = check - 1;
     }
 }
-// for (var item of document.querySelectorAll("main")) {
-//     item.addEventListener("click", function (evt) {
-//         evt.target.setAttribute("style", "background-color: green");
-//         evt.target.setAttribute("style", `opacity: ${b};`);
-//     }, false);
-// }
-
-
